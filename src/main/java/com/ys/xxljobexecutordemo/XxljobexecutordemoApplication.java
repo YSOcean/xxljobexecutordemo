2559ed0660c0cd6381b08ead1fd086b19dedb0c4
YSOcean <18271265732@163.com>
2019-03-16 23:30:33 +0800

    Initial commit

---
+++ src/main/java/com/ys/xxljobexecutordemo/XxljobexecutordemoApplication.java
@@ -0,0 +1,13 @@
+package com.ys.xxljobexecutordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxljobexecutordemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XxljobexecutordemoApplication.class, args);
+	}
+
+}
